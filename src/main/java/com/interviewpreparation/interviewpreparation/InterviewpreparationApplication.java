f5a67fe7acca8a3ac0b919f17c88326797b35f24
Kevin <Godwin.g@qbrainx.com>
2025-04-11 13:04:08 +0530

    Initial Commit

---
+++ src/main/java/com/interviewpreparation/interviewpreparation/InterviewpreparationApplication.java
@@ -0,0 +1,13 @@
+package com.interviewpreparation.interviewpreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewpreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewpreparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewpreparation/interviewpreparation/InterviewpreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewpreparation.interviewpreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewpreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
